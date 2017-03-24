5acdddac43bd7c4cbafb924a37821ebe55e0a61a
Simone Orsi <simahawk@gmail.com>
2017-03-24 09:14:22 +0100

    [add] cms_notifications

---
+++ cms_notifications/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'CMS notifications',
+    'summary': """Basic notifications management for your CMS system""",
+    'version': '9.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Camptocamp,Odoo Community Association (OCA)',
+    'depends': [
+        'cms_form',
+        'mail_digest',
+    ],
+    'data': [
+        'views/mail_message_subtype_views.xml',
+    ],
+}
